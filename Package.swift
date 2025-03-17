c97a533db5a64cec80432ca93efc5649a5e8e052
Vijay <vijay@Vijay-M-R-MacBook-Pro.local>
2025-03-17 17:00:31 +0530

    Update ezto framework

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+        // Targets can depend on other targets in this package and products from dependencies..
         .binaryTarget(
             name: "EztoVerify",
             path: "./EztoVerify.xcframework"
